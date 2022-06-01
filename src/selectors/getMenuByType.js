af54b392aae7a5365c785799034909264a717915
DavidRomeroC <romerocandelariodavid@gmail.com>
2022-05-31 21:01:45 -0500

    segundo commit

--- src/selectors/getMenuByType.js
+++ src/selectors/getMenuByType.js
@@ -5,7 +5,7 @@ export const getMenuByType = (type) => {
     const validMenu = ["taco", "agua", "tamal", "antojito"];
 
     if (!validMenu.includes(type)){
-        throw new Error(`${type} is not a valid publisher`)
+        throw new Error(`${type} is not a valid type of menu`);
     }
 
     return menu.filter(menu => menu.type === type);
